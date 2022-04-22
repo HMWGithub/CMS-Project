755e9b1a1595d328d2defe24faff14a1b5e6ddbe
Harrison Max-Wilson <H.MaxWilson@hotmail.com>
2022-04-22 23:03:53 +0100

    2nd Initial Commit
    Course starts a fresh for proper CMS section

--- mysql/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $connection = mysqli_connect('localhost', 'root', '', 'login_app');
-  if ($connection){
-    echo "connection successful";
-  } else {
-    die("connection failed");
-  }
-
-?>
